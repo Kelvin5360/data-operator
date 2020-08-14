99b567a22a432e117cd4ac1856b6ab5b9ff743aa
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-14 23:36:32 +0800

    as json before save to mongo

--- lib/data/operator/define_jobs.rb
+++ lib/data/operator/define_jobs.rb
@@ -1,7 +1,7 @@
 module DataOperator
     class DefineJob
         def self.run(data_job)
-            MongoDb.db[:data_jobs].insert_one(data_job)
+            MongoDb.db[:data_jobs].insert_one(data_job.as_json)
         end
     end
 end
