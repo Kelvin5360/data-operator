4c79ece2d57c7120fc2a0b547311c08686541342
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-14 19:02:25 +0800

    fix bug

--- app.rb
+++ app.rb
@@ -6,22 +6,18 @@ require 'active_support/core_ext'
 require 'require_all'
 require_all 'lib'
 
-module Data
-    module Operator
-        class App < Sinatra::Base
-            register Sinatra::Contrib
+module DataOperator
+    class App < Sinatra::Base
+        register Sinatra::Contrib
 
-            post '/api/data_jobs' do
-                payload =JSON.parse(request.body.read).symbolize_keys
-                DefineJob.run(DataJob.new(payload))
-                json(payload)
-            end
+        post '/api/data_jobs' do
+            payload =JSON.parse(request.body.read).symbolize_keys
+            DefineJob.run(DataJob.new(payload))
+            json(payload)
+        end
 
-            get '/api/data_jobs' do
-                json(MongoDb.db[:data_jobs].find({}))
-            end
-
-
+        get '/api/data_jobs' do
+            json(MongoDb.db[:data_jobs].find({}))
         end
     end
 end
--- lib/data/operator/define_jobs.rb
+++ lib/data/operator/define_jobs.rb
@@ -1,9 +1,8 @@
-module Data
-    module Operator
-        class DefineJob
-            def self.run(data_job)
-                MongoDb.db[data_jobs].insert_one(data_job)
-            end
+module DataOperator
+    class DefineJob
+        def self.run(data_job)
+            MongoDb.db[data_jobs].insert_one(data_job)
         end
     end
 end
+
