ebe02abf9cdb5df94b100432d3f6c891f935d673
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-14 23:41:22 +0800

    retrieve from db and trigger job

--- initializers/scheduler.rb
+++ initializers/scheduler.rb
@@ -3,6 +3,5 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
 scheduler.every '1s' do
-  # try trigger job
-  puts "job trigger running!!"
+  DataOperator::TriggerJobs.run
 end
--- lib/data/operator/trigger_jobs.rb
+++ lib/data/operator/trigger_jobs.rb
@@ -1,7 +1,10 @@
     module DataOperator
         class TriggerJobs
             def self.run
-                # MongoDb.db[jobs].
+                all_jobs = MongoDb.db[data_jobs].find({})
+                all_jobs.each do |job_spec|
+                    puts "triggering job #{job_spec[:name]}"
+                end
             end
         end
     end
