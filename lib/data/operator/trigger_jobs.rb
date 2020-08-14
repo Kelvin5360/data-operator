7cad2e256d3cbb7a2afc6aa4f73ed9f472d7ab16
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-14 23:50:41 +0800

    mook

--- lib/data/operator/trigger_jobs.rb
+++ lib/data/operator/trigger_jobs.rb
@@ -1,7 +1,7 @@
     module DataOperator
         class TriggerJobs
             def self.run
-                all_jobs = MongoDb.db[data_jobs].find({})
+                all_jobs = MongoDb.db[:data_jobs].find({})
                 all_jobs.each do |job_spec|
                     puts "triggering job #{job_spec[:name]}"
                 end
