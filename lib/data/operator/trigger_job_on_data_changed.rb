7c53bc4b9c85b68180131c81123bca2833a2fbaf
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-16 00:54:28 +0800

    with indifferent access

--- lib/data/operator/trigger_job_on_data_changed.rb
+++ lib/data/operator/trigger_job_on_data_changed.rb
@@ -1,7 +1,8 @@
 module DataOperator
     class TriggerJobOnDataChanged
         def self.run(data_changes)
-            data_changes.each do |data_change|
+            data_changes.each do |data_change_raw|
+                data_change = data_change_raw.with_indifferent_access
                 job_specs = MongoDb.db[:data_jobs].find({depends_on: data_change[:type]})
                 job_specs.each do |job_spec|
                     puts "triggering job #{job_spec[:name]}"
