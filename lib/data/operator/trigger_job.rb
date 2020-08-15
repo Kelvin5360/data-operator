f7fedfa3f805f38eabf7395fab0a9a3ef650b892
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-15 22:42:22 +0800

    fix trigger job bug

--- lib/data/operator/trigger_job.rb
+++ lib/data/operator/trigger_job.rb
@@ -1,9 +1,13 @@
     module DataOperator
         class TriggerJob
             def self.run(job_id, context)
-                job_spec = MongoDb.db[:data_jobs].find({name: job_id})
-                puts "triggering job #{job_spec[:name]}"
-                Faraday.post(job_spec[:url], (context.nil? ? {}: context).merge({job_id: job_spec[:name]}).to_json, 'Content-Type': 'application/json')
+                job_spec = MongoDb.db[:data_jobs].find({name: job_id}).first
+                if job_spec.nil?
+                    raise "job #{job_id} not exists!"
+                else
+                    puts "triggering job #{job_spec[:name]}"
+                    Faraday.post(job_spec[:url], (context.nil? ? {}: context).merge({job_id: job_spec[:name]}).to_json, 'Content-Type': 'application/json')
+                end
             end
         end
     end
