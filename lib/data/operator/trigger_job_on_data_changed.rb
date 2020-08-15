9cf64eae745ef4c16498097033e1c5e06e09b4ad
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-15 23:55:54 +0800

    fix syntex error

--- lib/data/operator/trigger_job_on_data_changed.rb
+++ lib/data/operator/trigger_job_on_data_changed.rb
@@ -2,10 +2,11 @@ module DataOperator
     class TriggerJobOnDataChanged
         def self.run(data_changes)
             data_changes.each do |data_change|
-            job_specs = MonsgoDb.db[:data_jobs].find({depends_on: data_change[:type]})
-            job_specs.each do |job_spec|
-                puts "triggering job #{job_spec[:name]}"
-                Faraday.post(job_spec[:url], data_change[:key].merge({job_id: job_spec[:name]}).to_json, 'Content-Type': 'application/json')
+                job_specs = MonsgoDb.db[:data_jobs].find({depends_on: data_change[:type]})
+                job_specs.each do |job_spec|
+                    puts "triggering job #{job_spec[:name]}"
+                    Faraday.post(job_spec[:url], data_change[:key].merge({job_id: job_spec[:name]}).to_json, 'Content-Type': 'application/json')
+                end
             end
         end
     end
