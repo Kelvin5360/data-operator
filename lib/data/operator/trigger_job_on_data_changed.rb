fd9f39f07583a335602bb80b292f6b7057108298
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-16 00:49:41 +0800

    fix name error

--- lib/data/operator/trigger_job_on_data_changed.rb
+++ lib/data/operator/trigger_job_on_data_changed.rb
@@ -2,7 +2,7 @@ module DataOperator
     class TriggerJobOnDataChanged
         def self.run(data_changes)
             data_changes.each do |data_change|
-                job_specs = MonsgoDb.db[:data_jobs].find({depends_on: data_change[:type]})
+                job_specs = MongoDb.db[:data_jobs].find({depends_on: data_change[:type]})
                 job_specs.each do |job_spec|
                     puts "triggering job #{job_spec[:name]}"
                     Faraday.post(job_spec[:url], data_change[:key].merge({job_id: job_spec[:name]}).to_json, 'Content-Type': 'application/json')
