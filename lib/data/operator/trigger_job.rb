18e65e945b7d2529579f55bc3920c561bf8eb158
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-15 19:28:04 +0800

    trigger job is id

--- lib/data/operator/trigger_job.rb
+++ lib/data/operator/trigger_job.rb
@@ -3,7 +3,7 @@
             def self.run(job_id)
                 job_spec = MongoDb.db[:data_jobs].find({name: job_id})                
                 puts "triggering job #{job_spec.name}"
-                Faraday.post(job_spec.url)
+                Faraday.post(job_spec.url, {job_id: job_spec.name}.to_json, 'Content-Type': 'application/json')
             end
         end
     end
