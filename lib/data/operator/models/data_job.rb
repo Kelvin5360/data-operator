fb73f4088e75c336ba919b8ce9a1019c16170c33
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-15 23:18:11 +0800

    add depends on to data job

--- lib/data/operator/models/data_job.rb
+++ lib/data/operator/models/data_job.rb
@@ -4,10 +4,11 @@ module DataOperator
     module Types
         include Dry.Types()
     end
-        
+
     class DataJob < Dry::Struct
         attribute :name, Types::String
         attribute :url, Types::String
         attribute :cron, Types::String
+        attribute :depends_on Types::Array.of(Types::Coercible::String)
     end
 end
