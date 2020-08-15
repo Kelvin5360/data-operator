cfa8c6b6b17c3f9b7f9a6c86f4d4b5f07e9b436f
kelvin5360@gmail.com <kelvin5360@gmail.com>
2020-08-15 22:34:51 +0800

    fix bug

--- lib/data/operator/trigger_job.rb
+++ lib/data/operator/trigger_job.rb
@@ -2,8 +2,8 @@
         class TriggerJob
             def self.run(job_id, context)
                 job_spec = MongoDb.db[:data_jobs].find({name: job_id})
-                puts "triggering job #{job_spec.name}"
-                Faraday.post(job_spec.url, (context.nil? ? {}: context).merge({job_id: job_spec.name}).to_json, 'Content-Type': 'application/json')
+                puts "triggering job #{job_spec[:name]}"
+                Faraday.post(job_spec[:url], (context.nil? ? {}: context).merge({job_id: job_spec[:name]}).to_json, 'Content-Type': 'application/json')
             end
         end
     end
